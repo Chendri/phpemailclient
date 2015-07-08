9bfe2d3f081241ef4fbee130f4470c67b8f38481
Chendri <hendrcor@gmail.com>
2015-07-08 11:54:37 -0500

    Modified inbox view to have clickable rows.

--- views/email/inbox.php
+++ views/email/inbox.php
@@ -6,12 +6,12 @@
    </thead>
    <tbody>
    <?php foreach($emails as $email):?>
-   <tr class="clickable-row" data-href="<?php echo base_url('email').'/read_message/'.$email->msgno?>">
+   <tr style="cursor:pointer;" class="clickable-row" data-href="<?php echo base_url('email').'/read_message/'.$email->msgno?>">
       <td><?php echo $email->from?></td>
       <td><?php echo $email->subject?></td>
-      <td><a href="<?php echo base_url('index.php/email').'/read_message/'.$email->uid?>">test</a></td>
    </tr>
    <?php endforeach;?>
    </tbody>
 </table>
 </div>
+<script src="application/js/inbox.js"></script>
