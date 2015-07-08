7e4cb5ff776658fabfcd6e9e242da7d7ae445a55
Chendri <hendrcor@gmail.com>
2015-07-08 12:00:40 -0500

    Fixed the go back button in the message view breaking the inbox

--- views/email/read_message.php
+++ views/email/read_message.php
@@ -2,5 +2,5 @@
 <?php echo quoted_printable_decode($body)?>
 
 <div class="container">
-<a href="<?php echo base_url().'index.php/email'?>" class="btn btn-default">Go Back<a/>
+<a href="<?php echo base_url().'email'?>" class="btn btn-default">Go Back<a/>
 </div>
