a27c8316849709d8a4942a8c601fd72672e02ac9
Chendri <hendrcor@gmail.com>
2015-07-08 11:38:33 -0500

    Updated client to properly handle MIME messages.

--- helpers/email_helper.php
+++ helpers/email_helper.php
@@ -1,10 +1,13 @@
 <?php
+require 'application/third_party/EmailMessage.php';
+//TODO Have the email client read off of some config file only reachable by the server
 if(!function_exists("open_stream"))
 {
-   function open_stream()
+   function open_stream($username = '', $password='')
    {
       //Having some certification problem, going to deal with it later.
       $hostname = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
+      //DEBUG SETTINGS
       $username = '';
       $password = '';
 
@@ -24,8 +27,37 @@ if(!function_exists("fetch_message"))
 {
    function fetch_message($client, $msgno)
    {
-      $message['header']        = imap_fetchheader($client, $msgno);
-      $message['body']          = imap_fetchbody($client, $msgno, 2);
-      return($message);
+      $emailMessage                     = new EmailMessage($client, $msgno);
+      $emailMessage->fetch();
+      process_inline($emailMessage);
+
+      $data['body'] = $emailMessage->bodyHTML;
+
+      return($data);
+   }
+}
+
+if(!function_exists("process_inline"))
+{
+   function process_inline($emailMessage)
+   {
+      $CG = get_instance();
+      $CG->load->helper('string');
+      preg_match_all('/src="cid:(.*)"/Uims', $emailMessage->bodyHTML, $matches);
+      if(count($matches)) {
+
+         $search = array();
+         $replace = array();
+
+         foreach($matches[1] as $match) {
+            $uniqueFilename = "cidattach".random_string().".jpg";
+            file_put_contents("images/$uniqueFilename", $emailMessage->attachments[$match]['data']);
+            $search[] = "src=\"cid:$match\"";
+            $replace[] = "src=\"http://emailclient.com/images/$uniqueFilename\"";
+         }
+
+         $emailMessage->bodyHTML = str_replace($search, $replace, $emailMessage->bodyHTML);
+
+      }
    }
 }
--- views/email/read_message.php
+++ views/email/read_message.php
@@ -1,3 +1,4 @@
+
 <?php echo quoted_printable_decode($body)?>
 
 <div class="container">
