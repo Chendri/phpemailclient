219a722af3442c545c448866e1ffea5d1a9377bb
Corey <hendrcor@gmail.com>
2015-07-09 10:06:47 -0500

    Fixed debug page, left in an s by accident

--- views/email/debug.php
+++ views/email/debug.php
@@ -1,5 +1,5 @@
 <?php echo '<pre>';var_dump($structure);'</pre>';?>
-<?php echo '<pre>';var_dump($headers);'</pre>';?>
+<?php echo '<pre>';var_dump($header);'</pre>';?>
 <?php echo '<pre>';var_dump($body);'</pre>';?>
 
 
